524da0bfc6951af1b9d4a9799d1695c95fceef61
qubi00 <jason1004qiu@gmail.com>
2024-10-07 23:24:01 -0400

    Fixed current_date

--- HW/HW3 Design/wildlife_tracker/animal_management/animal.py
+++ HW/HW3 Design/wildlife_tracker/animal_management/animal.py
@@ -5,7 +5,6 @@ class Animal:
     def __init__(self, 
                 animal_id: int,
                 species: str,
-                current_date: str,
                 age: Optional[int] = None,
                 health_status: Optional[str] = None,
                 animals: List[int] = []) -> None:
@@ -13,7 +12,6 @@ class Animal:
             self.animal_id = animal_id
             self.health_status = health_status
             self.species = species
-            self.current_date = current_date
             self.animals = animals or []
 
     def update_animal_details(self, animal_id: int, **kwargs: Any) -> None:
--- HW/HW3 Design/wildlife_tracker/migration_tracking/migration.py
+++ HW/HW3 Design/wildlife_tracker/migration_tracking/migration.py
@@ -6,12 +6,14 @@ class Migration:
                 migration_id: int,
                 migration_path: MigrationPath,
                 start_date: str,
+                current_date: str,
                 current_location: str,
                 status: str = "Scheduled"
                 )
         self.migration_id = migration_id
         self.migration_path = migration_path
         self.start_date = start_date
+        self.current_date = current_date
         self.status = status
         self.current_location = current_location
 
